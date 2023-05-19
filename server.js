db257563beecc798a90c8f06c9d77e73405b378e
Austin Henrie <nitsua15@gmail.com>
2023-05-18 21:00:36 -0600

    trying port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require("express")
 let api = express()
 api.use(express.static(__dirname + "/public"))
-api.listen(3001, function() {
-    console.log("on port 3001")
+api.listen(80, function() {
+    console.log("on port 80")
 })
